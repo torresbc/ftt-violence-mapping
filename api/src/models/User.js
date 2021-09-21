5b011448ae5f5be2e6142da3d26f1fdb38e52ea2
torresbc <bcristorres@gmail.com>
2021-09-20 21:00:08 -0300

    Creation: models structure

---
+++ api/src/models/User.js
@@ -0,0 +1,11 @@
+class User 
+{
+    constructor(id, name, email, contact, birth_date)
+    {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.contact = contact;
+        this.birth_date = birth_date;
+    }
+}
